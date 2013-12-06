cfdae63fdbdbad1e49db8ea5dee3089f4e59b5ed
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2013-12-06 17:59:29 +0530

    Updated event definitions

--- apache-stratos-sample-event-publisher/src/main/java/org/imesh/samples/apache/stratos/event/publisher/TopologyEvnetsPublisherMain.java
+++ apache-stratos-sample-event-publisher/src/main/java/org/imesh/samples/apache/stratos/event/publisher/TopologyEvnetsPublisherMain.java
@@ -30,8 +30,8 @@ import java.io.IOException;
  * Run this main class to send a set of sample topology events.
  */
 public class TopologyEvnetsPublisherMain {
-    private static String TOPIC_NAME = "topology-topic";
-    private static long TIME_INTERVAL = 10000; // 10 Seconds
+    private static String TOPIC_NAME = "topology";
+    private static long TIME_INTERVAL = 4000; // 4 Seconds
 
     public static void main(String[] args) {
         TopicPublisher publisher = new TopicPublisher(TOPIC_NAME);
@@ -54,33 +54,28 @@ public class TopologyEvnetsPublisherMain {
 
         // Application server service
         Service service1 = new Service("AppServer");
+        service1.addPort(new Port("https", 9764, 90));
         topology.addService(service1);
 
         // Application server cluster 1
-        Cluster cluster1 = new Cluster(service1.getServiceName(), "c1", "p1");
-        cluster1.setHostName("appserver.foo.org");
+        Cluster cluster1 = new Cluster(service1.getServiceName(), "appserver-cluster", "p1");
+        cluster1.addHostName("appserver.foo.org");
         cluster1.setTenantRange("1-*");
         service1.addCluster(cluster1);
 
         // Application server cluster 1 members
         Member member1 = new Member(cluster1.getServiceName(), cluster1.getClusterId(), "m1");
-        member1.setMemberIp("10.100.5.8");
-        member1.addPort(new Port("http", 9769, 8280));
-        member1.addPort(new Port("https", 9449, 8290));
+        member1.setMemberIp("10.0.0.1");
         member1.setStatus(MemberStatus.Activated);
         cluster1.addMember(member1);
 
         Member member2 = new Member(cluster1.getServiceName(), cluster1.getClusterId(), "m2");
-        member2.setMemberIp("10.100.5.8");
-        member2.addPort(new Port("http", 9770, 8280));
-        member2.addPort(new Port("https", 9450, 8290));
+        member2.setMemberIp("10.0.0.1");
         member2.setStatus(MemberStatus.Activated);
         cluster1.addMember(member2);
 
         Member member3 = new Member(cluster1.getServiceName(), cluster1.getClusterId(), "m3");
-        member3.setMemberIp("10.100.5.8");
-        member3.addPort(new Port("http", 9771, 8280));
-        member3.addPort(new Port("https", 9451, 8290));
+        member3.setMemberIp("10.0.0.1");
         member3.setStatus(MemberStatus.Activated);
         cluster1.addMember(member3);
 
@@ -92,31 +87,30 @@ public class TopologyEvnetsPublisherMain {
 
         // Send ESB service created event
         ServiceCreatedEvent event1 = new ServiceCreatedEvent("ESB");
+        event1.addPort(new Port("https", 9764, 90));
         publisher.publish(event1);
         Thread.sleep(TIME_INTERVAL);
 
         // Send ESB cluster c1 created event
-        ClusterCreatedEvent event2 = new ClusterCreatedEvent("ESB", "c1");
-        event2.setHostName("esb.foo.org");
+        ClusterCreatedEvent event2 = new ClusterCreatedEvent("ESB", "esb-cluster", "esb.foo.org");
         event2.setTenantRange("1-*");
         publisher.publish(event2);
         Thread.sleep(TIME_INTERVAL);
 
         // Send ESB cluster c1 member m1 spawned event
-        InstanceSpawnedEvent event3 = new InstanceSpawnedEvent("ESB", "c1", "m1", "p1");
+        InstanceSpawnedEvent event3 = new InstanceSpawnedEvent(event2.getServiceName(), event2.getClusterId(), "m1", "p1");
         publisher.publish(event3);
         Thread.sleep(TIME_INTERVAL);
 
         // Send ESB cluster c1 member m1 started event
-        MemberStartedEvent event4 = new MemberStartedEvent("ESB", "c1", "m1");
+        MemberStartedEvent event4 = new MemberStartedEvent(event2.getServiceName(), event2.getClusterId(), "m1");
         publisher.publish(event4);
         Thread.sleep(TIME_INTERVAL);
 
         // Send ESB cluster c1 member m1 activated event
-        MemberActivatedEvent event5 = new MemberActivatedEvent("ESB", "c1", "m1");
-        event5.setMemberIp("google.lk");
-        event5.addPort(new Port("http", 80, 8280));
-        event5.addPort(new Port("https", 90, 8290));
+        MemberActivatedEvent event5 = new MemberActivatedEvent(event2.getServiceName(), event2.getClusterId(), "m1");
+        event5.setMemberIp("10.0.0.1");
+        event5.addPort(new Port("http", 9764, 90));
         publisher.publish(event5);
         Thread.sleep(TIME_INTERVAL);
     }
