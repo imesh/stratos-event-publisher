317ebc0539a4aaf66d72599f2788681187269d96
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2014-04-17 11:39:28 +0530

    Implemented a new event generator

---
+++ sample-event-publisher/src/main/java/org/imesh/samples/apache/stratos/event/receiver/EventReceiver.java
@@ -0,0 +1,11 @@
+package org.imesh.samples.apache.stratos.event.receiver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: imesh
+ * Date: 4/17/14
+ * Time: 10:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EventReceiver {
+}
